79da2ce0b1b8f2251571ba69f76f13d697461670
sudeepbrana <sudeepbrana@gmail.com>
2020-05-16 21:09:51 +0545

    Chnaged laravel name to sudeep

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                  Sudeep
                 </div>
 
                 <div class="links">
